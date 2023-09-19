10cd95ef1b832722fc12f37c39695e36153d6e08
Diego Matias <dg.matias021@gmail.com>
2023-09-19 13:19:05 -0300

    criando o componente carrossel

---
+++ src/app/home/carrossel/destaque/destaque.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pro-destaque',
+  templateUrl: './destaque.component.html',
+  styleUrls: ['./destaque.component.css']
+})
+export class DestaqueComponent {
+
+}
