f66654febfcb0e7878f14376a1f582a1b1c5ba00
Diego Matias <dg.matias021@gmail.com>
2023-09-20 10:35:07 -0300

    atualização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CarrosselComponent } from './home/carrossel/carrossel.component';
+import { HeaderComponent } from './home/carrossel/header/header.component';
+import { DestaqueComponent } from './home/carrossel/destaque/destaque.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CarrosselComponent,
+    HeaderComponent,
+    DestaqueComponent,
   ],
   imports: [
     BrowserModule,
